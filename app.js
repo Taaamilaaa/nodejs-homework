9247f91c0edd84836ae266bb92189a6b2f96cfeb
Yudina Tamila <taaamilaaa1988@gmail.com>
2022-01-11 16:34:42 +0200

    add helmet & rate-limit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import logger from 'morgan'
 import cors from 'cors'
+import helmet from 'helmet'
+import { LIMIT_JSON } from './lib/constants'
 import contactsRouter from './routes/api/contacts'
 import authRouter from './routes/api/auth'
 
@@ -8,9 +10,10 @@ export const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
+app.use(helmet())
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json())
+app.use(express.json({limit: LIMIT_JSON}))
 
 app.use('/api/auth', authRouter)
 app.use('/api/contacts', contactsRouter)
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import controllers from "./controllers/auth"
 import guard from "../../midllewares/guard";
+import limiter from "../../midllewares/rate-limit";
 
 const router = Router();
 
-router.post("/signup", controllers.signup);
+router.post("/signup", limiter(15 * 60 * 1000, 2), controllers.signup);
 router.post("/login", controllers.login);
 router.post("/logout", guard, controllers.logout);
 router.get("/current", guard, controllers.current)
