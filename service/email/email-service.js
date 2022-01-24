525ce547ae83abf5ad9ad0d9617f98904752a0ad
Yudina Tamila <taaamilaaa1988@gmail.com>
2022-01-24 17:54:13 +0200

    server restart

--- service/email/email-service.js
+++ service/email/email-service.js
@@ -5,7 +5,7 @@ class EmailService {
     this.sender = sender
     switch (env) {
       case 'development':
-        this.link = 'http://localhost:8800/'
+        this.link = 'http://localhost:8900/'
         break
       case 'test':
         this.link = 'http://localhost:5000/'
@@ -14,7 +14,7 @@ class EmailService {
         this.link = 'http://heroku/'
         break
       default:
-        this.link = 'http://localhost:8800/'
+        this.link = 'http://localhost:8900/'
     }
   }
 
@@ -29,14 +29,14 @@ class EmailService {
 
     const email = {
       body: {
-        name: 'Dear user',
+        name: 'Dear USER))',
         intro: "Welcome! We're very excited to have you on board.",
         action: {
           instructions: "To get started with our API, please click here:",
           button: {
             color: "#22BC66", 
             text: "Confirm your account",
-            link: `${this.link}/api/users/verify/${verifyToken}`,
+            link: `${this.link}api/users/verify/${verifyToken}`,
           },
         },
         outro:
@@ -58,7 +58,7 @@ class EmailService {
         try {
             const result = await this.sender.send(message)
           console.log(result);
-          console.log(`${this.link}/api/users/verify/${verifyToken}`);
+
             return true
         } catch (error) {
             console.error(error.message)
